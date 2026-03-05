dd1ad64e4b16d2df2dc7ba792265f5f4a4c4a3a8
MAKWANADARSHAN <makwanadarshan6113@gmail.com>
2026-03-05 09:27:49 +0530

    219

---
+++ src/leetcode/ContainsDuplicate.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+    public boolean containsNearbyDuplicate1(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    if (Math.abs(i - j) <= k) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (Math.abs(i - map.get(nums[i])) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
