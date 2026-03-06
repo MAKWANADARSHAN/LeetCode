f36cdefa01a170a3094467cce3387bebc3bf9b85
MAKWANADARSHAN <makwanadarshan6113@gmail.com>
2026-03-06 10:10:55 +0530

    409

--- src/leetcode/LongestPalindrome.java
+++ src/leetcode/LongestPalindrome.java
@@ -6,16 +6,14 @@ public class LongestPalindrome {
         if (s.length() == 1) {
             return s.length();
         }
-        int[] count = new int[128];  // ASCII
+        int[] count = new int[128];
         int ans = 0;
         for (char c : s.toCharArray()) {
             count[c]++;
         }
         boolean oddFound = false;
         for (int ch : count) {
-
             ans += (ch / 2) * 2;
-
             if (ch % 2 == 1) {
                 oddFound = true;
             }
