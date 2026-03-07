4978df900d81b48fd773b49da718798bd2ed78b9
MAKWANADARSHAN <makwanadarshan6113@gmail.com>
2026-03-07 10:06:57 +0530

    680

---
+++ src/leetcode/ValidPalindrome.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+public class ValidPalindrome {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+            if (ch1 != ch2) {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int i, int j) {
+
+        while (i <= j) {
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+            if (ch1 != ch2) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
